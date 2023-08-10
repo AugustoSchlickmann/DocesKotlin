2ad84a2af581fb61c082c84a9d65c37fd5d7b02a
augustoschlickmann <augusto3625@hotmail.com>
2023-08-10 16:48:17 -0300

    Aplicando Herança e Polimorfismo nos Adapters e Fragments

--- app/src/main/java/com/augusto/doceskotlin/objetos/Doce.kt
+++ app/src/main/java/com/augusto/doceskotlin/objetos/Doce.kt
@@ -3,33 +3,33 @@ package com.augusto.doceskotlin.objetos
 import android.os.Parcel
 import android.os.Parcelable
 
-data class Doce(var idDoce: Long?, var nomeDoce: String?, var imagemDoce: String?) : Parcelable {
+data class Doce(var idDoce: Long?, var nomeDoce: String?, var imagemDoce: String?, var valorDoce: Double?) : Parcelable{
 
     var quantidadeDoce = 0
-    var valorDoce: Double? = null
 
     constructor(parcel: Parcel) : this(
         parcel.readValue(Long::class.java.classLoader) as? Long,
         parcel.readString(),
-        parcel.readString()
+        parcel.readString(),
+        parcel.readValue(Double::class.java.classLoader) as? Double
     ) {
         quantidadeDoce = parcel.readInt()
-        valorDoce = parcel.readValue(Double::class.java.classLoader) as? Double
-    }
-
-    constructor() : this(null, null, null)
-
-    override fun toString(): String {
-        return "Doce: (idDoce=$idDoce, nomeDoce=$nomeDoce, imagemDoce=$imagemDoce, valorDoce=$valorDoce, quantidadeDoce=$quantidadeDoce)"
     }
 
     override fun writeToParcel(parcel: Parcel, flags: Int) {
         parcel.writeValue(idDoce)
         parcel.writeString(nomeDoce)
         parcel.writeString(imagemDoce)
-        parcel.writeInt(quantidadeDoce)
         parcel.writeValue(valorDoce)
+        parcel.writeInt(quantidadeDoce)
     }
+
+    constructor() : this(null, null, null, null)
+
+    override fun toString(): String {
+        return "Doce: (idDoce=$idDoce, nomeDoce=$nomeDoce, imagemDoce=$imagemDoce, valorDoce=$valorDoce, quantidadeDoce=$quantidadeDoce)"
+    }
+
 
     override fun describeContents(): Int {
         return 0
